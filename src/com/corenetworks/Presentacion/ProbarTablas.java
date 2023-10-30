dfde63bc2628a4eb38ab8b2c770a8870e6ed205d
DanielGuerreroRa <guerreroraezdaniel@hotmail.com>
2023-10-30 19:53:03 +0100

    Initial commit

---
+++ src/com/corenetworks/Presentacion/ProbarTablas.java
@@ -0,0 +1,19 @@
+package com.corenetworks.modelo;
+
+import com.corenetworks.Presentacion.TablasDeMultimplicar;
+
+import java.util.Scanner;
+
+public class ProbarTablas {
+    public static void main(String[] args) {
+        TablasDeMultimplicar t1 =new TablasDeMultimplicar();
+        for (int i = 1; i <=10; i++){
+            = t1 * i;
+        }
+        t1.setTablas();
+
+
+
+
+    }
+}
---
+++ src/com/corenetworks/modelo/TablaMultimplicar.java
@@ -0,0 +1,17 @@
+package com.corenetworks.Presentacion;
+
+public class TablasDeMultimplicar {
+    private int tablas;
+    private String tablasMultiplicar;
+
+    public void setTablas (int tablas){
+        this.tablas=tablas;
+    }
+   public void setTablasMultiplicar(String tablasMultiplicar){
+        this.tablasMultiplicar=tablasMultiplicar;
+   }
+
+    public String getTablasMultiplicar() {
+        return tablasMultiplicar;
+    }
+}
